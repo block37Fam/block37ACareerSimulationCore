eea63b12021f1fe66b1242452bd2ad22d340ffb2
ConnorWotkowicz <wottkowicz@gmail.com>
2025-04-01 12:06:47 -0400

    Added requests to logregRoutes.js

--- server/api/logregRoutes.js
+++ server/api/logregRoutes.js
@@ -7,6 +7,27 @@ const {
   loginUser 
 } = require('./db');
 
-// API routes
+
+router.post('/register', async (req, res, next) => {
+    try {
+      const { username, email, password } = req.body;
+      const newUser = await registerUser({ username, email, password });
+      res.status(201).send(newUser);
+    } catch (err) {
+      next(err);
+    }
+  });
+  
+
+  router.post('/login', async (req, res, next) => {
+    try {
+      const { email, password } = req.body;
+      const user = await loginUser(email, password);
+      res.send(user);
+    } catch (err) {
+      next(err);
+    }
+  });
+  
 
 module.exports = router;
