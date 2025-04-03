f556516471bf11cb495e464a31511066633182d6
Yue Xie <moonxie9304@gmail.com>
2025-04-03 20:34:49 +0700

    added user review management router

--- server/api.js
+++ server/api.js
@@ -5,8 +5,10 @@ const router = express.Router();
 const logregRouter = require('./api/logregRoutes')
 const userMangement = require('./api/userManagement')
 const itemManagement = require('./api/itemManagement')
+const userReviewManagement = require('./api/userReviewManagement')
 router.use('/auth', logregRouter)
 router.use('/auth', userMangement)
 router.use('/items', itemManagement)
+router.use('/review', userReviewManagement)
 
 module.exports = router;
