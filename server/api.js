b72a6d6fb9bd03cde0ddd7c6be09522f98eb5f1a
Yue Xie <moonxie9304@gmail.com>
2025-04-03 09:23:20 +0700

    added item management routers

--- server/api.js
+++ server/api.js
@@ -4,7 +4,9 @@ const router = express.Router();
 // API routes
 const logregRouter = require('./api/logregRoutes')
 const userMangement = require('./api/userManagement')
+const itemManagement = require('./api/itemManagement')
 router.use('/auth', logregRouter)
 router.use('/auth', userMangement)
+router.use('items', itemManagement)
 
 module.exports = router;
--- server/api/itemManagement.js
+++ server/api/itemManagement.js
@@ -3,9 +3,26 @@ const router = express.Router();
 
 const {
     getAllItems,
-    getItemByID
+    getItemById
 } = require('../db');
 
 // API routes
+router.get('/', async(req, res, next) => {
+    try {
+        const response = await getAllItems()
+        res.status(200).send(response)
+    } catch (error) {
+        next(error)
+    }
+})
 
+router.get('/:itemId', async(req, res, next) => {
+    try {
+        const itemId = req.params.itemId
+        const response = await getItemById(itemId)
+        res.status(200).send(response)
+    } catch (error) {
+        next(error)
+    }
+})
 module.exports = router;
